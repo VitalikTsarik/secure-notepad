33e0a7def626df5a1d27b9110372735763713930
MisterOlrox <nikitakholdobin@yandex.ru>
2020-10-08 20:04:44 +0300

    upd6

--- src/test/java/com/delivery/controller/MainControllerTest.java
+++ src/test/java/com/delivery/controller/MainControllerTest.java
@@ -17,7 +17,7 @@ public class MainControllerTest {
     @Test
     public void test() throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
         MainController controller = new MainController();
-        controller.getSessionKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVAFUAKu4ip9BhmdSwyuqE7dGjV1qzXurVBhIKtMDYksarlT6awnL08LVsEyDleVCuhWxBZtBGUw8GmL3EiDFeAxlKtX4xPAiGm1JLPFevggZbwD75nYVzYAOW57QfXReYFl++Fnjw+PLjHjP7jdLe6BawuuoT+DNEOBjSWh6HkQIDAQAB");
+        //controller.getSessionKey("MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVAFUAKu4ip9BhmdSwyuqE7dGjV1qzXurVBhIKtMDYksarlT6awnL08LVsEyDleVCuhWxBZtBGUw8GmL3EiDFeAxlKtX4xPAiGm1JLPFevggZbwD75nYVzYAOW57QfXReYFl++Fnjw+PLjHjP7jdLe6BawuuoT+DNEOBjSWh6HkQIDAQAB");
     }
 
 }
